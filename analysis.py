36a68988d64fce8fa376991bddf83b48b440476a
Lukasz Swierkowski <G00439343@atu.ie>
2025-04-21 20:01:02 +0100

    Add iris analysis and dataset

---
+++ analysis.py
@@ -0,0 +1,21 @@
+# Fisher’s Iris data set Project
+# Objectives
+# A. Fisher in 1936, it contains measurements of iris flowers from three different species: Iris setosa, Iris versicolor, and Iris virginica. Each sample includes four features:
+
+
+
+# Author: Lukasz Swierkowski
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the dataset
+df = df = pd.read_csv('C:/Users/luke_/OneDrive/Desktop/pands/pands-project/iris.csv')y:")
+
+print(df.describe())
+
+
+
+
+
