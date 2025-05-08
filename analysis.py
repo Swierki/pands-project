d4b247dc932ed7cb48fbb055cbeed2000278aa7d
Lukasz Swierkowski <G00439343@atu.ie>
2025-05-08 09:22:06 +0100

    Initial commit: Iris similarity project

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 # Fisher’s Iris data set Project
 # Objectives
+# Iris Species Similarity Index
 # A. Fisher in 1936, it contains measurements of iris flowers from three different species: Iris setosa, Iris versicolor, and Iris virginica. Each sample includes four features:
 
 
@@ -14,6 +15,15 @@ import seaborn as sns
 df = df = pd.read_csv('C:/Users/luke_/OneDrive/Desktop/pands/pands-project/iris.csv')y:")
 
 print(df.describe())
+
+
+# This project compares similarities in the  index between the three Iris species by:
+# Measuring how close their average feature vectors are.
+# Using Euclidean distance and cosine similarity between species centroids
+# Visualizing similarity using a dendrogram or heatmap.
+
+
+
 
 
 
